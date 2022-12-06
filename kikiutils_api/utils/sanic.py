13a5a2e08954b2b55800f237c3d1fec8ab8bce46
kiki-kanri <a470666@gmail.com>
2022-12-06 18:48:06 +0800

    Set sanic validate support get method

--- kikiutils_api/utils/sanic.py
+++ kikiutils_api/utils/sanic.py
@@ -1,13 +1,15 @@
 from sanic import Request
 
 
-def get_request_data(rq: Request):
+def get_request_data(rq: Request) -> dict[str]:
     try:
-        request_data: dict = rq.json
+        if (request_data := rq.json) is None:
+            raise ValueError()
     except:
         request_data = {}
+        rq_kwargs = rq.form or rq.args
 
-        for k in rq.form:
-            request_data[k] = rq.form.get(k)
+        for k in rq_kwargs:
+            request_data[k] = rq_kwargs.get(k)
 
     return request_data
